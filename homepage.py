03b007d881e6ea8c69dd8e7e692a966a9b544881
TeongTat <102958559+TeongTat@users.noreply.github.com>
2025-03-03 15:20:03 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -15,6 +15,9 @@ st.set_page_config(layout="wide")
 #set tittle page
 st.title("Stock Predictor App")
 
+# Create tabs
+tab1, tab2 = st.tabs(["S&P 500 Stock Info", "Price Prediction"])
+
 # Load an image
 image_path = "stock_market_banner_970x250.png"
 image = Image.open(image_path)
@@ -29,6 +32,5 @@ The app will display the following:
 
 The main purpose of this application is providing price guidance for investors on the price risks and market risks of the S&P 500 stocks.""")
 
-# Create tabs
-tab1, tab2 = st.tabs(["S&P 500 Stock Info", "Price Prediction"])
+
 
