245257c98083d231c25c1b9bafb8f70482cbdb4a
TeongTat <102958559+TeongTat@users.noreply.github.com>
2025-03-03 15:58:32 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -29,8 +29,8 @@ with tab1:
   st.write("""
   This is a stock price and prediction modelling app that will assist investors on buying or selling the stocks. The stocks are based on all companies listed on S&P 500 and the price are up to date linking from Yahoo Finance server.
   The app will display the following:
-  - Historical stock price trend up to the latest.
-  - Showcase stock price forecast (up to 10days).
+  - Historical stock price trend up to the latest : =tab2
+  - Showcase stock price forecast (up to 5days) : =tab3
   
   The main purpose of this application is providing price guidance for investors on the price risks and market risks of the S&P 500 stocks.""")
 
