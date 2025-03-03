95f47197d256a04015270ddf8acd470f77253b38
TeongTat <102958559+TeongTat@users.noreply.github.com>
2025-03-03 15:21:34 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -16,21 +16,25 @@ st.set_page_config(layout="wide")
 st.title("Stock Predictor App")
 
 # Create tabs
-tab1, tab2 = st.tabs(["S&P 500 Stock Info", "Price Prediction"])
+tab1, tab2, tab3 = st.tabs(["Introduction", "S&P 500 Stock Info", "Price Prediction"])
 
-# Load an image
-image_path = "stock_market_banner_970x250.png"
-image = Image.open(image_path)
-st.image(image, use_container_width=True)
+# Add content to Tab 1
+with tab1:
+  # Load an image
+  image_path = "stock_market_banner_970x250.png"
+  image = Image.open(image_path)
+  st.image(image, use_container_width=True)
+  
+  
+  st.write("""
+  This is a stock price and prediction modelling app that will assist investors on buying or selling the stocks. The stocks are based on all companies listed on S&P 500 and the price are up to date linking from Yahoo Finance server.
+  The app will display the following:
+  - Historical stock price trend up to the latest.
+  - Showcase stock price forecast (up to 10days).
+  
+  The main purpose of this application is providing price guidance for investors on the price risks and market risks of the S&P 500 stocks.""")
 
-
-st.write("""
-This is a stock price and prediction modelling app that will assist investors on buying or selling the stocks. The stocks are based on all companies listed on S&P 500 and the price are up to date linking from Yahoo Finance server.
-The app will display the following:
-- Historical stock price trend up to the latest.
-- Showcase stock price forecast (up to 10days).
-
-The main purpose of this application is providing price guidance for investors on the price risks and market risks of the S&P 500 stocks.""")
-
+# Add content to Tab 2
+with tab2:
 
 
